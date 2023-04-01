8e2355ea95687416634cc746f1809578a5220bcf
Diana Yovbak <dianayovbak@Mac-Diana.local>
2023-04-01 13:10:31 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,36 @@
 // Підключаємо технологію express для back-end сервера
+const { text } = require('express')
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
+
+// ================================================================
+
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '“600$ в місяц',
+  address: 'Uzhhorod, Ukraine',
+}
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
 
 // ================================================================
 
@@ -16,17 +45,190 @@ router.get('/', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
-// ================================================================
-
-//              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/summary', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
+  //
   res.render('summary', {
-    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+      },
+    },
+
+    footer,
   })
 })
 
-// ================================================================
+router.get('/skills', function (req, res) {
+  res.render('skills', {
+    page: {
+      title: 'Resume | Skills',
+    },
 
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebears',
+          point: 8,
+          isTop: false,
+        },
+        {
+          name: 'Git',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS code',
+          point: 8,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+
+      hobbies: [
+        {
+          name: 'reading',
+          isMain: true,
+        },
+        {
+          name: 'painting',
+          isMain: false,
+        },
+        {
+          name: 'climbing',
+          isMain: false,
+        },
+      ],
+    },
+
+    // footer,
+  })
+})
+router.get('/education', function (req, res) {
+  res.render('education', {
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'Uzhnu',
+          isEnd: true,
+        },
+        {
+          name: 'Svl',
+          isEnd: true,
+        },
+        {
+          name: 'Svlms',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'AAA',
+          id: 699,
+        },
+        {
+          name: 'BBB',
+          id: 9329,
+        },
+        {
+          name: 'CCC',
+          id: 4543,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/work', function (req, res) {
+  res.render('work', {
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://web.skillzrun.com/learn/115266',
+          },
+          duration: {
+            from: '10/10/2023',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://web.skillzrun.com/learn/115266',
+              about: 'lalalalala',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML | CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              stacksAmount: 4,
+              awards: [
+                {
+                  name: 'lol',
+                },
+                {
+                  name: 'lil',
+                },
+              ],
+              awardAmoGnt: 7,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
